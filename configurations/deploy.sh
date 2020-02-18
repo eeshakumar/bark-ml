59debe0c2c69fe75b9fe226be50505e8ce031ea6
ChenyangTang <tcy1079006289@gmail.com>
2020-02-18 23:59:59 +0100

    initial setup

--- configurations/deploy.sh
+++ configurations/deploy.sh
@@ -4,8 +4,8 @@ echo "Building $.."
 bazel build //configurations/$1:configuration
 
 echo "Uploading.."
-rsync bazel-bin/configurations/$1/ 8gpu:/mnt/glusterdata/home/$2/$3 -a --copy-links -v -z -P
-rsync ./configurations/run.sh 8gpu:/mnt/glusterdata/home/$2/$3/run.sh -a --copy-links -v -z -P
+rsync bazel-bin/configurations/$1/ $2@8gpu:/mnt/glusterdata/home/$2/$3 -a --copy-links -v -z -P
+rsync ./configurations/run.sh $2@8gpu:/mnt/glusterdata/home/$2/$3/run.sh -a --copy-links -v -z -P
 
 # ssh 8gpu && cd $3 && sbatch run.sh
 # echo "Training started!"
