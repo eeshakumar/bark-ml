9239d56d9c771fdf0eb3341df7563b7654c316c4
Eesha Kumar <mee.59912011@gmail.com>
2020-10-13 01:31:35 +0200

    Per with demonstrator data

--- bark_ml/library_wrappers/lib_fqf_iqn_qrdqn/memory/__init__.py
+++ bark_ml/library_wrappers/lib_fqf_iqn_qrdqn/memory/__init__.py
@@ -1,2 +1,4 @@
 from .base import LazyMemory, LazyMultiStepMemory
 from .per import LazyPrioritizedMultiStepMemory
+from .base_dem import LazyDemMemory, LazyDemMultiStepMemory
+from .per_dem import LazyPrioritizedDemMultiStepMemory
