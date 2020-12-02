9b168f2a7f16687f149e772628e99de156077fad
Eesha Kumar <mee.59912011@gmail.com>
2020-12-02 17:41:47 +0100

    Some cleanup and addition of reset operations

--- bark_ml/library_wrappers/lib_fqf_iqn_qrdqn/memory/base_dem.py
+++ bark_ml/library_wrappers/lib_fqf_iqn_qrdqn/memory/base_dem.py
@@ -33,22 +33,49 @@ class LazyDemMemory(LazyMemory):
     is_demo_keys = ['is_demo']
     keys = state_keys + np_keys + is_demo_keys
 
-    def __init__(self, capacity, state_shape, device, demo_ratio=0.25):
-        super().__init__(capacity, state_shape, device)
-        # print("State shape", self.state_shape, *self.state_shape)
+    def reset_indexing(self, capacity, state_shape, device,
+                       demo_ratio=0.25):
         self.demo_ratio = demo_ratio
         self.demo_capacity = int(demo_ratio*capacity)
         self.agent_capacity = capacity - self.demo_capacity
-        self._dn = 0
+        if demo_ratio < 1.0:
+            # assume memory already contains demo samples
+            self._dn = self.demo_capacity - 1
+            self._n = self._dn
+            self._p = self._dn
+        else:
+            self._dn = 0
         self._an = self.demo_capacity
+
+    def is_full(self):
+        return self._n == self.capacity
+
+    def reset_offline(self, capacity, state_shape, device, demo_ratio):
+        self.reset_indexing(capacity, state_shape, device, demo_ratio)
+        self['state'][self._an:self.capacity] = np.zeros(
+            (self.agent_capacity, *state_shape), dtype=np.float32)
+        self['next_state'][self._an:self.capacity] = np.zeros(
+            (self.agent_capacity, *state_shape), dtype=np.float32)
+        self['action'][self._an:self.capacity] = np.empty(
+            (self.agent_capacity, 1), dtype=np.int64)
+        self['reward'][self._an:self.capacity] = np.empty(
+            (self.agent_capacity, 1), dtype=np.float32)
+        self['done'][self._an:self.capacity] = np.empty(
+            (self.agent_capacity, 1), dtype=np.float32)
+        self['is_demo'][self._an: self.capacity] = np.zeros(
+            (self.agent_capacity, 1), dtype=np.int8)
+
+    def __init__(self, capacity, state_shape, device, demo_ratio=0.25):
+        super().__init__(capacity, state_shape, device)
+        self.reset_indexing(capacity, state_shape, device, demo_ratio)
 
     def reset(self, is_demo=True):
         super().reset()
         # assume at initialization all memory samples are demo samples
         if is_demo:
-            self['is_demo'] = np.ones((self.capacity, 1), dtype=np.int64)
+            self['is_demo'] = np.ones((self.capacity, 1), dtype=np.int8)
         else:
-            self['is_demo'] = np.zeros((self.capacity, 1), dtype=np.int64)
+            self['is_demo'] = np.zeros((self.capacity, 1), dtype=np.int8)
         self['state'] = np.zeros((self.capacity, *self.state_shape))
         self['next_state'] = np.zeros((self.capacity, *self.state_shape))
 
@@ -56,10 +83,7 @@ class LazyDemMemory(LazyMemory):
         self._append(state, action, reward, next_state, done, is_demo)
 
     def _append(self, state, action, reward, next_state, done, is_demo):
-        # print("_append")
         if is_demo:
-            # print("DN", self._dn)
-            # print("D ST , MEM ST", state.shape, self['state'].shape, self['state'][self._dn].shape)
             self['state'][self._dn] = state
             self['next_state'][self._dn] = next_state
             self['action'][self._dn] = action
@@ -67,44 +91,27 @@ class LazyDemMemory(LazyMemory):
             self['done'][self._dn] = done
             self['is_demo'][self._dn] = is_demo
             self._dn = (self._dn + 1) % self.demo_capacity
-            # print("DN AF", self._dn)
         else:
-            # print("ND ST , MEM ST", state.shape, self['state'].shape, self['state'][self._an].shape)
             self['state'][self._an] = state
             self['next_state'][self._an] = next_state
             self['action'][self._an] = action
             self['reward'][self._an] = reward
             self['done'][self._an] = done
             self['is_demo'][self._an] = is_demo
-            # print("AN", self._an)
             if self._an - self.demo_capacity >= (self.agent_capacity - 1):
                 self._an = self.demo_capacity
             else:
                 self._an = (self._an + 1)
-            # print("AN AF", self._an)
-
-        # self['state'].append(state)
-        # self['next_state'].append(next_state)
-        # self['state'][self._p] = state
-        # self['next_state'][self._p] = next_state
-        # self['action'][self._p] = action
-        # self['reward'][self._p] = reward
-        # self['done'][self._p] = done
-        # self['is_demo'][self._p] = is_demo
 
         self._n = min(self._n + 1, self.capacity)
         self._p = (self._p + 1) % self.capacity
-        # print("N P DN AN", self._n, self._p, self._dn, self._an)
         self.truncate()
 
     def _sample(self, indices, batch_size):
-        # bias = -self._p if self._n == self.capacity else 0
         states = np.empty((batch_size, *self.state_shape), dtype=np.float32)
         next_states = np.empty((batch_size, *self.state_shape), dtype=np.float32)
 
         for i, index in enumerate(indices):
-            # bias is annealed in importance sampling, so there is no use for it here
-            # _index = np.mod(index + bias, self.capacity)
             states[i, ...] = self['state'][index]
             next_states[i, ...] = self['next_state'][index]
 
@@ -129,12 +136,9 @@ class LazyDemMultiStepMemory(LazyDemMemory):
             self.buff = MultiStepDemBuff(maxlen=self.multi_step)
 
     def append(self, state, action, reward, next_state, done, is_demo):
-        # print("Append to memory reward/is_demo", reward, is_demo)
         if self.multi_step != 1:
             self.buff.append(state, action, reward, is_demo)
             if self.buff.is_full():
-                #TODO: priority change
-                # print("Buff is full, get and append to replay")
                 state, action, reward, is_demo = self.buff.get(self.gamma)
                 self._append(state, action, reward, next_state, done, is_demo)
             if done:
