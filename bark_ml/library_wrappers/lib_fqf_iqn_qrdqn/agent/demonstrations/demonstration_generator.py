414edfa52f442c43332cabdf68f70d14bc9574bc
Eesha Kumar <mee.59912011@gmail.com>
2020-12-03 13:12:53 +0100

    Wrapper to generate demonstrations

---
+++ bark_ml/library_wrappers/lib_fqf_iqn_qrdqn/agent/demonstrations/demonstration_generator.py
@@ -0,0 +1,35 @@
+import os
+
+class DemonstrationGenerator(object):
+
+    def __init__(self, env, params, behavior, collector, save_dir):
+        self._env = env
+        self._params = params
+        self._behavior = behavior
+        self._collector = collector
+        self._save_dir = save_dir
+        self._demonstrations = None
+
+    @property
+    def demonstrations(self):
+        return self._demonstrations
+
+    def generate_demonstrations(self, num_episodes, eval_criteria, save_dir=None,
+                                gen_file="generated_demonstrations"):
+        if save_dir is None:
+            save_dir = self._save_dir
+
+        demo_path = os.path.join(save_dir, gen_file)
+        collection_result = self._collector.CollectDemonstrations(
+            self._env, self._behavior, num_episodes, demo_path, use_mp_runner=False,
+            runner_init_params={"deepcopy": False})
+        self._collector.ProcessCollectionResult(eval_criteria)
+        self._demonstrations = self._collector.GetDemonstrationExperiences()
+        return self._demonstrations
+
+    def dump_demonstrations(self, save_dir=None, gen_file="generated_demonstrations"):
+        if save_dir is None:
+            save_dir = self._save_dir
+        demo_path = os.path.join(save_dir, gen_file)
+        print("Demonstrations will be saved to", demo_path)
+        self._collector.dump(demo_path)
